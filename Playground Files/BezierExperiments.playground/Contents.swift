015f6da3f541bd6caf71e04ccbd997374a979f56
Denis <fpv.airplane.barnaul@gmail.com>
2022-09-04 22:00:16 +0300

    Знакомство с кривыми Безье

--- Playground Files/BezierExperiments.playground/Contents.swift
+++ Playground Files/BezierExperiments.playground/Contents.swift
@@ -6,22 +6,19 @@ import CoreGraphics
 
 private func getPath() -> UIBezierPath {
     let path = UIBezierPath()
-    path.move(to: CGPoint(x: 50, y: 50))
-    path.addLine(to: CGPoint(x: 150, y: 50))
-    path.addLine(to: CGPoint(x: 150, y: 150))
-    path.close()
-//    path.addLine(to: CGPoint(x: 350, y: 150))
-//    path.addLine(to: CGPoint(x: 250, y: 50))
+    path.move(to: CGPoint(x: 10, y: 10))
+    path.addCurve(to: CGPoint(x: 200, y: 200),
+    controlPoint1: CGPoint(x: 200, y: 20), controlPoint2: CGPoint(x: 20, y: 200))
     return path
 }
 
 private func createBezier(on view: UIView) {
     let shapeLayer = CAShapeLayer()
     view.layer.addSublayer(shapeLayer)
-    shapeLayer.strokeColor = UIColor.red.cgColor
+    shapeLayer.strokeColor = UIColor.gray.cgColor
     shapeLayer.lineWidth = 5
     shapeLayer.path = getPath().cgPath
-    shapeLayer.fillColor = UIColor.green.cgColor
+    shapeLayer.fillColor = UIColor.systemGreen.cgColor
     shapeLayer.lineCap = .round
     shapeLayer.lineJoin = .round
 //    shapeLayer.lineDashPattern = [10]
